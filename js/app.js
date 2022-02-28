05045cd09672b318085b115b0bde809f200e3934
Anna Kovalik <annakovalik1@gmail.com>
2022-02-28 16:14:08 +0300

    Add function to transform slides

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ const slides = document.querySelectorAll(".section__container__item");
 const lastSlide = slides.length - 1;
 let currentSlide = 0;
 const dotsContainer = document.querySelector(".dots");
+
+// place works slides
+
+function goToSlide(slide) {
+  slides.forEach((sl, index) => {
+    sl.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+}
+
+goToSlide(0);
